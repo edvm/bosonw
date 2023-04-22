ec5c6cecdac0fe80563c02b5f40d25bfa573d325
Emiliano Dalla Verde Marcozzi <edvm@fedoraproject.org>
2023-04-22 18:10:11 -0400

    API to ask weather is working

---
+++ web-service-gin/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s"
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/weather", getWeather)
+
+	router.Run("localhost:8080")
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// API request
+func getWeather(c *gin.Context) {
+	lat, long := c.Query("lat"), c.Query("long")
+	url := fmt.Sprintf(apiUrl, lat, long)
+
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	fmt.Printf("Body: %s\n", body)
+	c.IndentedJSON(http.StatusOK, string(body))
+}
